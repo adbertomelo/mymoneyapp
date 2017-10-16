1e9b622496312a71f8959c00ee6050c0b0e72c98
Beto <adbertomelo@gmail.com>
2017-10-16 05:09:45 -0300

    commit

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -15,11 +15,36 @@ module.exports = {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
-            jquery: '',
-            bootstrap:''
-        },
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap:'modules/admin-lte/bootstrap/js/bootstrap.js'
+        }
+    },
     plugins:[
-        
-    ]
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'windows.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
+        loaders: [
+            {
+                test: /.js[x]?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015','react'],
+                    plugins: ['transform-object-rest-spread']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader','css-loader')
+            },
+            {
+                test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+                loader:'file'
+            }
+        ]}
     }
-}
